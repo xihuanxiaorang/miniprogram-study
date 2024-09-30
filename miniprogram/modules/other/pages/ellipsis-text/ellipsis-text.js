c062f1c1393bb2a430d36d002a4d46fdf42de15e
xiaorang <15019474951@163.com>
2024-09-30 19:01:10 +0800

    feat(minicode): 完成文本内容超出指定行数后自动省略并显示省略号的代码片段页面

---
+++ miniprogram/modules/other/pages/ellipsis-text/ellipsis-text.js
@@ -0,0 +1,72 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    topMVList: [
+      {
+        id: 22618702,
+        cover: 'http://p1.music.126.net/YskN07NSTYz0MWRJdzoD8A==/109951169987227781.jpg',
+        name: '蒙顶千秋',
+        artistName: 'VaVa毛衍七',
+      },
+      {
+        id: 22609057,
+        cover: 'http://p1.music.126.net/5nHSmM80mqy2zYhSUMXMUw==/109951169891995007.jpg',
+        name: '毛不易 《冒险精神》专辑同名主打曲 「Official Music Video」',
+        artistName: '毛不易',
+      },
+      {
+        id: 22601098,
+        cover: 'http://p1.music.126.net/nR0dFE50SKF3OZUqdrn5jw==/109951169764807080.jpg',
+        name: 'TOOOOO BUSY',
+        artistName: '王霏霏',
+      },
+      {
+        id: 22617981,
+        cover: 'http://p1.music.126.net/h2hq9pgANEOrD20-IqOrvQ==/109951169973863654.jpg',
+        name: '偷心',
+        artistName: '胡歌',
+      },
+      {
+        id: 22610011,
+        cover: 'http://p1.music.126.net/bjqxw9BvJW-LacByzOcCHw==/109951169892068620.jpg',
+        name: 'STEP',
+        playCount: 15379,
+        briefDesc: null,
+        desc: null,
+        artistName: '张艺兴',
+      },
+      {
+        id: 22608363,
+        cover: 'http://p1.music.126.net/5rJYzzlpkBr77xCSBfXtoA==/109951169869348651.jpg',
+        name: '这世界我来过',
+        artistName: '宝石Gem',
+      },
+      {
+        id: 22604242,
+        cover: 'http://p1.music.126.net/314yfYtIp1MerE52GQdPcw==/109951169806550950.jpg',
+        name: '馈赠',
+        artistName: 'RICHNOMADIC',
+      },
+      {
+        id: 22610284,
+        cover: 'http://p1.music.126.net/ovCljkqw4CShNDpi8mUWbQ==/109951169898081224.jpg',
+        name: '酸酸甜甜真的我',
+        artistName: '檀健次',
+      },
+      {
+        id: 22605419,
+        cover: 'http://p1.music.126.net/s6RF6YUeF9j6yio_YOxpfg==/109951169812269688.jpg',
+        name: '云上叨叨EP21 独家专访 | h3R3刘清云',
+        artistName: 'h3R3',
+      },
+      {
+        id: 22615146,
+        cover: 'http://p1.music.126.net/1VzSjnM40gG_1zQvqr3alg==/109951169946827435.jpg',
+        name: '毛不易 《只在今夜 Serenade》Official Music Video',
+        artistName: '毛不易',
+      },
+    ],
+  },
+})
